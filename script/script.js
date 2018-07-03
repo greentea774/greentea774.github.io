06d66bc7fa66bab012b64c9711d093bf6f2fac6a
greentea774 <vortex774@gmail.com>
2018-07-04 00:13:30 +0900

    add jquery JavaScript, make animation

---
+++ script/script.js
@@ -0,0 +1,56 @@
+// 各リンク先のマウスオン時bottom_messageを表示する
+// Twitter
+$("#items_twitter").hover(
+    function(){
+        /* mouse enter の処理 */
+        $('.bottom_message_twitter').fadeIn();
+    }, 
+    function(){
+        /* mouse leave の処理 */
+        $('.bottom_message_twitter').css('display','none');
+    }
+);
+// GitHub
+$("#items_github").hover(
+    function(){
+        /* mouse enter の処理 */
+        $('.bottom_message_github').fadeIn();
+    }, 
+    function(){
+        /* mouse leave の処理 */
+        $('.bottom_message_github').css('display','none');
+    }
+);
+// myprofile
+$("#items_myprofile").hover(
+    function(){
+        /* mouse enter の処理 */
+        $('.bottom_message_myprofile').fadeIn();
+    }, 
+    function(){
+        /* mouse leave の処理 */
+        $('.bottom_message_myprofile').css('display','none');
+    }
+);
+// product
+$("#items_product").hover(
+    function(){
+        /* mouse enter の処理 */
+        $('.bottom_message_product').fadeIn();
+    }, 
+    function(){
+        /* mouse leave の処理 */
+        $('.bottom_message_product').css('display','none');
+    }
+);
+// blog
+$("#items_blog").hover(
+    function(){
+        /* mouse enter の処理 */
+        $('.bottom_message_blog').fadeIn();
+    }, 
+    function(){
+        /* mouse leave の処理 */
+        $('.bottom_message_blog').css('display','none');
+    }
+);
